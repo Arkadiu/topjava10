10d9e0edabd26392d44b635295f5533644fb6b46
Arkadiu <ntfs2002@gmail.com>
2017-03-27 21:50:45 +0300

    HW01 meals jsp (first win over the web)

---
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -0,0 +1,38 @@
+package ru.javawebinar.topjava.web;
+
+import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.MealWithExceed;
+import ru.javawebinar.topjava.util.MealsUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalTime;
+import java.util.List;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ * Created by Alexander on 24.03.2017.
+ */
+public class MealServlet extends HttpServlet {
+    private static final Logger LOG = getLogger(MealServlet.class);
+
+    private List<MealWithExceed> list =
+            MealsUtil.getFilteredWithExceeded(MealsUtil.getMealIml(),
+                    LocalTime.of(7, 0),
+                    LocalTime.of(12, 0),
+                    2000);
+
+    @Override
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
+        LOG.debug("redirect to Meals");
+
+        request.setAttribute("meals", list);
+        getServletContext().getRequestDispatcher("/meals.jsp").forward(request, resp);
+
+    }
+}
---
+++ src/main/webapp/meals.jsp
@@ -0,0 +1,36 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Alexander
+  Date: 24.03.2017
+  Time: 16:32
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html>
+<head>
+    <title>Meal list</title>
+</head>
+<body>
+<h2><a href="index.html">Home</a></h2>
+
+<table border="1">
+    <caption>Таблица калорий</caption>
+    <tr>
+        <th> DateTime </th>
+        <th> Description </th>
+        <th> Calories </th>
+        <th> Exceed </th>
+    </tr>
+    <c:forEach items="${meals}" var="meal">
+        <tr>
+            <td>${meal.dateTime}</td>
+            <td>${meal.description}</td>
+            <td>${meal.calories}</td>
+            <td>${meal.exceed}</td>
+        </tr>
+    </c:forEach>
+</table>
+
+</body>
+</html>
