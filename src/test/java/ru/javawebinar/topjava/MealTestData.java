a1e8686709dfea8a48e48adfe90247d512422904
Arkadiu <ntfs2002@gmail.com>
2017-04-13 21:51:10 +0300

    passed 7 simple @test

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -4,25 +4,34 @@ import ru.javawebinar.topjava.matcher.ModelMatcher;
 import ru.javawebinar.topjava.model.Meal;
 
 import java.time.LocalDateTime;
-import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Objects;
 
 import static ru.javawebinar.topjava.model.BaseEntity.START_SEQ;
 
 public class MealTestData {
 
-    public static final int MEAL_ID = START_SEQ;
+    public static final int MEAL_ID = START_SEQ ;
+    public static int count = 2;
 
-    public static final List<Meal> MEALS = Arrays.asList(
-            new Meal(MEAL_ID, LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500),
-            new Meal(MEAL_ID + 1, LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000)
-            /*,new Meal(MEAL_ID + 2, LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500),
-            new Meal(MEAL_ID + 3, LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000),
-            new Meal(MEAL_ID + 4, LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500),
-            new Meal(MEAL_ID + 5, LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510)*/
-    );
+    public static final int MEAL100002_ID = MEAL_ID + count++;
+    public static final int MEAL100003_ID = MEAL_ID + count++;
+    public static final int MEAL100004_ID = MEAL_ID + count++;
+    public static final int MEAL100005_ID = MEAL_ID + count++;
+    public static final int MEAL100006_ID = MEAL_ID + count++;
+    public static final int MEAL100007_ID = MEAL_ID + count++;
+
+    public static final Meal MEAL100002 =
+            new Meal(MEAL100002_ID, LocalDateTime.of(2015, 5, 30, 10, 0), "Завтрак", 500);
+    public static final Meal MEAL100003 =
+            new Meal(MEAL100003_ID, LocalDateTime.of(2015, 5, 30, 13, 0), "Обед", 1000);
+    public static final Meal MEAL100004 =
+            new Meal(MEAL100004_ID, LocalDateTime.of(2015, 5, 30, 20, 0), "Ужин", 500);
+    public static final Meal MEAL100005 =
+            new Meal(MEAL100005_ID, LocalDateTime.of(2015, 5, 31, 10, 0), "Завтрак", 500);
+    public static final Meal MEAL100006 =
+            new Meal(MEAL100006_ID, LocalDateTime.of(2015, 5, 31, 13, 0), "Обед", 1000);
+    public static final Meal MEAL100007 =
+            new Meal(MEAL100007_ID, LocalDateTime.of(2015, 5, 31, 20, 0), "Ужин", 510);
 
     public static final ModelMatcher<Meal> MATCHER = new ModelMatcher<>(
             ((expected, actual) -> expected == actual ||
--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -1,6 +1,5 @@
 package ru.javawebinar.topjava.service;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,17 +7,18 @@ import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import ru.javawebinar.topjava.MealTestData;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.util.DateTimeUtil;
 import ru.javawebinar.topjava.util.DbPopulator;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.Month;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-import static ru.javawebinar.topjava.MealTestData.MATCHER;
-import static ru.javawebinar.topjava.MealTestData.MEAL_ID;
+import static java.time.LocalDateTime.now;
+import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
 /**
@@ -41,50 +41,78 @@ public class MealServiceTest {
     @Autowired
     private DbPopulator dbPopulator;
 
-    private List<Meal> MEALS;
+    private List<Meal> MEALS = new ArrayList<>();
 
     @Before
     public void setUp() throws Exception {
         dbPopulator.execute();
-        MEALS = new ArrayList<>(MealTestData.MEALS);
+        MEALS = Stream.of(MEAL100002,
+                MEAL100003,
+                MEAL100004,
+                MEAL100005,
+                MEAL100006,
+                MEAL100007)
+                .sorted(Comparator.comparing(Meal::getDateTime).reversed())
+                .collect(Collectors.toList());
     }
 
     @Test
     public void save() throws Exception {
-        Meal newMeal = new Meal(LocalDateTime.of(2007, Month.NOVEMBER, 10, 20, 0), "Ужин", 790);
+        Meal newMeal = new Meal(now(), "Тест-Ужин", 790);
         Meal created = service.save(newMeal, USER_ID);
         newMeal.setId(created.getId());
-        MEALS.add(newMeal);
+        MEALS.add(0, newMeal);
         MATCHER.assertCollectionEquals(MEALS, service.getAll(USER_ID));
     }
 
     @Test
     public void get() throws Exception {
-        Meal meal = service.get(MEAL_ID, USER_ID);
-        Assert.assertEquals(MEALS.get(0), meal);
+        Meal meal = service.get(MEAL100007_ID, USER_ID);
+        MATCHER.assertEquals(MEALS.get(0), meal);
     }
 
     @Test
     public void delete() throws Exception {
-        service.delete(MEAL_ID, USER_ID);
+        service.delete(MEAL100007_ID, USER_ID);
         MEALS.remove(0);
         MATCHER.assertCollectionEquals(service.getAll(USER_ID), MEALS);
     }
 
     @Test
     public void getBetweenDates() throws Exception {
+        LocalDate start = LocalDate.of(2015, 5, 30);
+        LocalDate end = LocalDate.of(2015, 5, 30);
+        Collection<Meal> filtered = MEALS.stream()
+                .filter(meal -> DateTimeUtil.isBetween(meal.getDate(), start, end))
+                .collect(Collectors.toList());
+        MATCHER.assertCollectionEquals(filtered, service.getBetweenDates(start, end, USER_ID));
     }
 
     @Test
     public void getBetweenDateTimes() throws Exception {
+        LocalDateTime start = LocalDateTime.of(2015, 5, 30, 9, 0);
+        LocalDateTime end = LocalDateTime.of(2015, 5, 30, 13, 0);
+        Collection<Meal> filtered = MEALS.stream()
+                .filter(meal -> DateTimeUtil.isBetween(meal.getDateTime(), start, end))
+                .collect(Collectors.toList());
+        MATCHER.assertCollectionEquals(filtered, service.getBetweenDateTimes(start, end, USER_ID));
     }
 
     @Test
     public void getAll() throws Exception {
+        Collection<Meal> all = service.getAll(USER_ID);
+        MATCHER.assertCollectionEquals(MEALS, all);
     }
 
     @Test
     public void update() throws Exception {
+        Meal updated = new Meal();
+        updated.setCalories(MEALS.get(0).getCalories());
+        updated.setDateTime(MEALS.get(0).getDateTime());
+        updated.setDescription(MEALS.get(0).getDescription());
+        updated.setId(MEALS.get(0).getId());
+        service.update(updated, USER_ID);
+        MATCHER.assertEquals(updated, service.get(MEALS.get(0).getId(), USER_ID));
     }
 
 }
