b39ba14ec7fdf17337e7ce9c38578417461b78d4
Arkadiu <ntfs2002@gmail.com>
2017-04-22 09:37:52 +0300

    HW04 not learning

--- src/main/java/ru/javawebinar/topjava/Main.java
+++ src/main/java/ru/javawebinar/topjava/Main.java
@@ -7,6 +7,7 @@ package ru.javawebinar.topjava;
  * @link http://caloriesmng.herokuapp.com/
  * @link https://github.com/JavaOPs/topjava
  */
+//HW04
 public class Main {
     public static void main(String[] args) {
         System.out.format("Hello Topjava Enterprise!");
